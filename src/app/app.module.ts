52c9b5f9fd1044f8b39ef6a3fcb0a4096136c0b5
VickyMutai <vicky.mutai96@gmail.com>
2020-09-16 22:46:42 +0300

    setting up the homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
