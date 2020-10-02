8dde3cafd5f5faa897927c054f9aafccffa154b4
VickyMutai <vicky.mutai96@gmail.com>
2020-10-02 12:49:15 +0300

    update on the required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { AngularFireModule } from "@angular/fire";
+// import { AngularFireAuthModule } from "@angular/fire/auth";
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +11,12 @@ import { NavComponent } from './nav/nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginPageComponent } from './registration/login-page/login-page.component';
+import { RegisterComponent } from './registration/register/register.component';
+import { ForgotPasswordComponent } from './registration/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './registration/verify-email/verify-email.component';
+import { ClientComponent } from './admin/client/client.component';
+import { UsersComponent } from './admin/users/users.component';
+import { ItemsComponent } from './dashboard/items/items.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +25,19 @@ import { LoginPageComponent } from './registration/login-page/login-page.compone
     DashboardComponent,
     FooterComponent,
     LoginPageComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    ClientComponent,
+    UsersComponent,
+    ItemsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAuthModule,
+    // AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAhpcS9zZuNcxQDOfsuup8COK1TGz2VSo0",
+    authDomain: "sambai-stores.firebaseapp.com",
+    databaseURL: "https://sambai-stores.firebaseio.com",
+    projectId: "sambai-stores",
+    storageBucket: "sambai-stores.appspot.com",
+    messagingSenderId: "294514924078",
+    appId: "1:294514924078:web:570b55cddb5bdd65991dbf",
+    measurementId: "G-GFCGS9GY82"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
